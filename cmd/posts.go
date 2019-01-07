c0f8a59074d82093568cddde373560bdf5839ced
scriptonist <aravindkp@outlook.in>
2019-01-07 14:01:23 +0530

    simple table tables

--- cmd/posts.go
+++ cmd/posts.go
@@ -1,9 +1,31 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/alexeyco/simpletable"
 	"github.com/spf13/cobra"
+)
+
+const (
+	rootAPIURL = "https://hashnode.com/ajax/posts"
+)
+
+// API URL's
+var (
+	hotPostsAPI = fmt.Sprintf("%s/%s", rootAPIURL, "hot")
+	newsAPI     = fmt.Sprintf("%s/%s", rootAPIURL, "news")
+)
+
+// flags
+var (
+	hot  bool
+	news bool
 )
 
 // postsCmd represents the posts command
@@ -11,10 +33,194 @@ var postsCmd = &cobra.Command{
 	Use:   "posts",
 	Short: "Lists posts",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("posts called")
+		switch {
+		case news:
+			getNews()
+		case hot:
+			getHotPosts()
+		default:
+			log.Println("Specify what posts to get")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(postsCmd)
+
+	postsCmd.PersistentFlags().BoolVar(&hot, "hot", false, "get hot posts")
+	postsCmd.PersistentFlags().BoolVar(&news, "news", false, "get news")
+}
+
+func getHotPosts() {
+	b, err := makeRequest(hotPostsAPI)
+	if err != nil {
+		log.Println(err)
+	}
+	var hotposts HotPosts
+	err = json.Unmarshal(b, &hotposts)
+	if err != nil {
+		log.Println(err)
+	}
+	table := simpletable.New()
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Title"},
+			{Align: simpletable.AlignCenter, Text: "Link"},
+		},
+	}
+	for ind, post := range hotposts.Posts {
+		r := []*simpletable.Cell{
+			{Align: simpletable.AlignRight, Text: fmt.Sprintf("%d", ind+1)},
+			{Align: simpletable.AlignRight, Text: post.Title},
+			{Align: simpletable.AlignRight, Text: post.Slug},
+		}
+		table.Body.Cells = append(table.Body.Cells, r)
+	}
+	table.SetStyle(simpletable.StyleCompactLite)
+	fmt.Println(table.String())
+}
+
+func getNews() {
+	b, err := makeRequest(newsAPI)
+	if err != nil {
+		log.Println(err)
+	}
+	m := make(map[string]interface{})
+	json.Unmarshal(b, &m)
+	fmt.Println(m)
+}
+
+func makeRequest(url string) ([]byte, error) {
+	client := http.Client{
+		Timeout: time.Duration(1 * time.Minute),
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		// read response body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return body, nil
+	}
+
+	return nil, fmt.Errorf("Not Found")
+
+}
+
+type HotPosts struct {
+	Posts []struct {
+		ID             string `json:"_id"`
+		FollowersCount int    `json:"followersCount"`
+		Author         struct {
+			ID            string      `json:"_id"`
+			Role          interface{} `json:"role"`
+			NumFollowing  int         `json:"numFollowing"`
+			NumFollowers  int         `json:"numFollowers"`
+			Name          string      `json:"name"`
+			Tagline       string      `json:"tagline"`
+			Photo         string      `json:"photo"`
+			Username      string      `json:"username"`
+			Appreciations []struct {
+				Badge string `json:"badge"`
+				ID    string `json:"_id"`
+				Count int    `json:"count"`
+			} `json:"appreciations"`
+			DateJoined  time.Time `json:"dateJoined"`
+			SocialMedia struct {
+				Website  string `json:"website"`
+				Twitter  string `json:"twitter"`
+				Github   string `json:"github"`
+				Linkedin string `json:"linkedin"`
+				Google   string `json:"google"`
+				Facebook string `json:"facebook"`
+			} `json:"socialMedia"`
+			StoriesCreated       []string      `json:"storiesCreated"`
+			Location             string        `json:"location"`
+			CoverImage           string        `json:"coverImage"`
+			BadgesAwarded        []interface{} `json:"badgesAwarded"`
+			TotalUpvotesReceived int           `json:"totalUpvotesReceived"`
+			IsEvangelist         bool          `json:"isEvangelist"`
+			NumReactions         int           `json:"numReactions"`
+		} `json:"author"`
+		Cuid                   string        `json:"cuid"`
+		Slug                   string        `json:"slug"`
+		Title                  string        `json:"title"`
+		Type                   string        `json:"type"`
+		ReactionsByCurrentUser []interface{} `json:"reactionsByCurrentUser"`
+		TotalReactions         int           `json:"totalReactions"`
+		Reactions              []struct {
+			ID    string `json:"_id"`
+			Image string `json:"image"`
+			Name  string `json:"name"`
+		} `json:"reactions"`
+		BookmarkedIn []interface{} `json:"bookmarkedIn"`
+		HasReward    bool          `json:"hasReward"`
+		Contributors []struct {
+			User struct {
+				ID            string        `json:"_id"`
+				Username      string        `json:"username"`
+				Name          string        `json:"name"`
+				Photo         string        `json:"photo"`
+				Tagline       string        `json:"tagline"`
+				BadgesAwarded []interface{} `json:"badgesAwarded"`
+				Appreciations []struct {
+					Badge string `json:"badge"`
+					ID    string `json:"_id"`
+					Count int    `json:"count"`
+				} `json:"appreciations"`
+				DateJoined  time.Time `json:"dateJoined"`
+				SocialMedia struct {
+					Twitter       string `json:"twitter"`
+					Github        string `json:"github"`
+					Stackoverflow string `json:"stackoverflow"`
+					Linkedin      string `json:"linkedin"`
+					Google        string `json:"google"`
+					Website       string `json:"website"`
+				} `json:"socialMedia"`
+				StoriesCreated       []string    `json:"storiesCreated"`
+				NumFollowing         int         `json:"numFollowing"`
+				NumFollowers         int         `json:"numFollowers"`
+				Location             string      `json:"location"`
+				Role                 interface{} `json:"role"`
+				CoverImage           string      `json:"coverImage"`
+				TotalUpvotesReceived int         `json:"totalUpvotesReceived"`
+				IsEvangelist         bool        `json:"isEvangelist"`
+				NumReactions         int         `json:"numReactions"`
+			} `json:"user"`
+			Stamp string `json:"stamp"`
+			ID    string `json:"_id"`
+		} `json:"contributors"`
+		IsActive      bool      `json:"isActive"`
+		ResponseCount int       `json:"responseCount"`
+		DateAdded     time.Time `json:"dateAdded"`
+		Tags          []struct {
+			ID         string      `json:"_id"`
+			Name       string      `json:"name"`
+			Slug       string      `json:"slug"`
+			MergedWith interface{} `json:"mergedWith,omitempty"`
+			IsApproved bool        `json:"isApproved"`
+			IsActive   bool        `json:"isActive"`
+		} `json:"tags"`
+		Downvotes               int           `json:"downvotes"`
+		Upvotes                 int           `json:"upvotes"`
+		TotalPollVotes          int           `json:"totalPollVotes"`
+		PollOptions             []interface{} `json:"pollOptions"`
+		HasPolls                bool          `json:"hasPolls"`
+		Brief                   string        `json:"brief"`
+		CoverImage              string        `json:"coverImage"`
+		Views                   int           `json:"views"`
+		IsAnonymous             bool          `json:"isAnonymous"`
+		DateUpdated             time.Time     `json:"dateUpdated"`
+		IndexVotedByCurrentUser int           `json:"indexVotedByCurrentUser"`
+		IsFollowing             bool          `json:"isFollowing"`
+		DateFeatured            time.Time     `json:"dateFeatured,omitempty"`
+	} `json:"posts"`
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/hashnode/hashnode-cli
 
 require (
+	github.com/alexeyco/simpletable v0.0.0-20180729223640-1fa9009f1080
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/mitchellh/go-homedir v1.0.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.1
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
